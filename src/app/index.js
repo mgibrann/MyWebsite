cb045c48fc4339ea3d57a0fd2bfe3405e5bda267
Muhamad Gibran <49028961+M-Gibran@users.noreply.github.com>
2020-12-22 19:26:28 +0700

    change to styled component

---
+++ src/app/index.js
@@ -0,0 +1,11 @@
+import { Text, Container } from "./index.style";
+
+const App = () => {
+  return (
+    <Container>
+      <Text>Coming Soon...</Text>
+    </Container>
+  );
+};
+
+export default App;
---
+++ src/app/index.style.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #282c34;
+`;
+
+export const Text = styled.text`
+  color: white;
+  font-size: 30px;
+  font-family: code;
+`;
