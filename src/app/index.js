e1f4efc467d18920f945d12b487f57e2b7d3e494
Muhamad Gibran <49028961+M-Gibran@users.noreply.github.com>
2020-12-23 21:30:03 +0700

    add header

--- src/app/index.js
+++ src/app/index.js
@@ -3,6 +3,12 @@ import { Text, Container } from "./index.style";
 const App = () => {
   return (
     <Container>
+      <header>
+        <ul>
+          <li>About Me</li>
+          <li>Contact</li>
+        </ul>
+      </header>
       <Text>Coming Soon...</Text>
     </Container>
   );
